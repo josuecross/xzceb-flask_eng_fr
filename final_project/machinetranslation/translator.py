dad27246e0159bc72a7305c3f6e557447d27f189
Josue Cruz <cruzlopez.josuedavid96@gmail.com>
2023-05-20 21:41:41 -0400

    Added function to connect to watson

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -8,3 +8,12 @@ load_dotenv()
 
 apikey = os.environ['apikey']
 url = os.environ['url']
+
+authenticator = IAMAuthenticator(apikey)
+
+lenguage_translator = LanguageTranslatorV3(version="2018-05-01", authenticator=authenticator)
+
+lenguage_translator.set_service_url(url)
+
+
+
